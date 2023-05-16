01851955c3bc38f106b851cf3aec84ccb5aa19d3
putubagus13 <putu031216@gmail.com>
2023-05-16 17:10:00 +0700

    fixed the error message and success message

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,44 +1,98 @@
 import ToyFace2 from "../assets/ToyFaces2.png"
 import ToyFace1 from "../assets/ToyFaces1.png"
-import { BsWhatsapp, BsFacebook } from "react-icons/bs"
+import { BsWhatsapp, BsFacebook, BsCheckCircleFill } from "react-icons/bs"
 import { AiFillTwitterCircle, AiFillInstagram } from "react-icons/ai"
 import {Link} from "react-router-dom"
 import {IoTicketSharp} from "react-icons/io5"
-import React from 'react';
-import { Formik } from 'formik';
-import * as Yup from 'yup'
+import React from "react"
+import { Formik } from "formik"
+import * as Yup from "yup"
 import http from "../helper/http"
 import {MdError} from "react-icons/md"
+import propTypes from "prop-types"
 
 const validationSchema = Yup.object({
-    email: Yup.string().email("Email is invalid").required("Email is invalid"),
-    password: Yup.string().required("Password is invalid")
+    email: Yup.string().email("Email is invalid").required("Email is invalid")
 })
 
+const FormRegister = (
+    {values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting, 
+    errorMessage, 
+    successMessage})=>{
+    return(
+        <form id="form" onSubmit={handleSubmit}>
+            {errorMessage && (<div className="flex flex-row justify-center alert alert-error shadow-lg text-white text-lg mb-3"><MdError size={30}/>{errorMessage}</div>)}
+            {successMessage && (<div className="flex flex-row justify-center alert alert-info shadow-lg text-white text-lg mb-3"><BsCheckCircleFill size={30}/>{successMessage}</div>)}
+            <div className="form-control flex flex-col">
+                <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="Email" 
+                    className= {`input input-bordered ${errors.email && touched.email && "input-error"} text-secondary h-14 w-full border-2 rounded-2xl px-5`}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                />
+                {errors.email && touched.email && (
+                    <label className="label">
+                        <span className="label-text-alt text-error">{errors.email}</span>
+                    </label>)
+                }
+            </div>
+            <button disabled={isSubmitting} className="my-2 h-14 w-full btn btn-primary rounded-2xl shadow-lg" type="submit" >Send</button>
+        </form>
+    )
+}
+FormRegister.propTypes = {
+    values: propTypes.objectOf(propTypes.string),
+    errors: propTypes.objectOf(propTypes.string), 
+    touched: propTypes.objectOf(propTypes.bool), 
+    handleChange: propTypes.func,
+    handleBlur: propTypes.func,
+    handleSubmit: propTypes.func,  
+    isSubmitting: propTypes.bool,
+    errorMessage: propTypes.string, 
+    successMessage: propTypes.string
+}
+
 function ForgotPassword(){
-    const [message, setMessage] = React.useState([])
-    const [showMessage, setShowMessage] = React.useState("")
-    React.useEffect(()=>{
-        const RequestForgotPass = async()=>{
-            try {
-                const {data} = await http().post("/forgotRequest")
-                console.log(data)
-                setMessage(data.results)
-            } catch (error) {
-                const message = error?.response?.data?.message
-                if(message){
-                console.log(message)
+    const [successMessage, setSuccessMessage] = React.useState("")
+    const [errorMessage, setErrorMessage] = React.useState("")
+
+    const requestForgotPass = async(values, {setSubmitting, setErrors})=>{
+        setErrorMessage("")
+        setSuccessMessage("")
+        try {
+            const body = new URLSearchParams(values).toString()
+            const {data} = await http().post("/auth/forgotRequest", body)
+            console.log(data)
+            setSuccessMessage(data.message)
+            setSubmitting(false)
+
+        } catch (error) {
+            const message = error?.response?.data?.message
+            if(message){
+                if(error?.response?.data?.results){
+                    setErrors({
+                        email: error.response.data.results.filter(item => item.param === "email")[0].message,
+                    })
+                }else{
+                    setErrorMessage(message)
                 }
             }
         }
-        RequestForgotPass()
-    })
-
-    function ShowRequest(){
-        if(message){
-            setShowMessage(message)
-        }
     }
+    React.useEffect(()=>{
+        console.log(errorMessage)
+    },[errorMessage])
+    
+
     return(
         <div>
          <main className="flex h-[1024px]">
@@ -57,41 +111,14 @@ function ForgotPassword(){
                     <h1 className="text-[24px] font-bold text-secondary" >Forgot Password</h1>
                     <p className="flex pb-6 pt-3 text-secondary">You’ll get mail soon on your email</p>
                     <Formik
-                        initialValues={{ email: '', password: '' }}
+                        initialValues={{ email: "" }}
                         validationSchema = {validationSchema}
-                        onSubmit={ ShowRequest}
+                        onSubmit={requestForgotPass}
                         >
-                        {({
-                            values,
-                            errors,
-                            touched,
-                            handleChange,
-                            handleBlur,
-                            handleSubmit,
-                            isSubmitting,
-                        }) => (
-                            <form id="form" onSubmit={handleSubmit}>
-                                {showMessage && (<div className="flex flex-row justify-center alert alert-error shadow-lg text-white text-lg"><MdError size={30}/>{showMessage}</div>)}
-                                <div className="form-control flex flex-col">
-                                    <input 
-                                        type="email" 
-                                        name="email" 
-                                        placeholder="email" 
-                                        className= {`input input-bordered ${errors.email && touched.email && "input-error"} text-secondary h-14 w-full border-2 rounded-2xl px-5`}
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        value={values.email}
-                                    />
-                                    {errors.email && touched.email && (
-                                        <label className="label">
-                                            <span className="label-text-alt text-error">{errors.email}</span>
-                                        </label>)
-                                    }
-                                </div>
-                                <button className="my-2 h-14 w-full btn btn-primary rounded-2xl shadow-lg" type="submit" disabled={isSubmitting}>Send</button>
-                            </form>
+                        {(props) => (
+                            <FormRegister {...props} errorMessage={errorMessage} successMessage={successMessage}/>
                         )}
-                        </Formik>
+                    </Formik>
                 </div>
             </main>
             <footer className="h-[476px] px-[30px] md:px-[20%] w-full md:py-[60px]">
