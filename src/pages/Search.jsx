4581a54a0cb5fe8ebafae52187cec10b9709fd55
putubagus13 <putu031216@gmail.com>
2023-07-18 08:04:34 +0700

    feat: input search, sortBy, limit

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,11 +3,11 @@ import { useNavigate, useSearchParams } from "react-router-dom"
 import http from "../helper/http"
 import { Link } from "react-router-dom"
 import moment from "moment"
-import {  BsWhatsapp , BsFacebook } from "react-icons/bs"
+import {  BsWhatsapp , BsFacebook , BsFilterLeft } from "react-icons/bs"
 import {
         AiFillTwitterCircle, 
         AiFillInstagram } from "react-icons/ai"
-import {FiMenu, FiLogOut} from "react-icons/fi"
+import {FiMenu, FiLogOut, FiSearch} from "react-icons/fi"
 import {SiArtixlinux} from "react-icons/si"
 import { useDispatch, useSelector } from "react-redux"
 import { logout } from "../redux/reducers/auth"
@@ -17,16 +17,28 @@ function Search(){
     const dispatch = useDispatch()
     const token = useSelector(state => state.auth.token)
     const [searchParams] = useSearchParams()
+    const searchValue = searchParams.get('search')
+    console.log(searchValue)
     const [searchResults, setSearcResults] = React.useState([])
     const [profile, setProfile] = React.useState({})
+    const [search, setSearch] = React.useState('')
+    const [limit, setLimit] = React.useState('')
+    const [sortBy, setSortBy] = React.useState('')
+    console.log(sortBy)
 
-    React.useEffect(()=>{
-        async function getSearchEvent(){
-            const {data} = await http().get("/events", {params: searchParams})
-            console.log(data)
+    async function getSearchEvent(searchValue, search, limit, sortBy){
+        if(!search){
+            const {data} = await http().get(`/events?search=${searchValue}&limit=${limit}&sortBy=${sortBy}`)
+            setSearcResults(data.results)
+        }else{
+            searchParams.toString('')
+            const {data} = await http().get(`/events?search=${search}&limit=${limit}&sortBy=${sortBy}`)
             setSearcResults(data.results)
         }
-        getSearchEvent()
+    } 
+
+    React.useEffect(()=>{
+        getSearchEvent(search, limit, sortBy, searchValue)
 
         async function getProfileUser(){
             try {
@@ -41,7 +53,7 @@ function Search(){
             }
         }
         getProfileUser()
-    },[])
+    },[token, search, searchParams, limit, sortBy, searchValue])
 
     function doLogout(){
         dispatch(logout())
@@ -86,7 +98,32 @@ function Search(){
             </nav>
             <main className="w-full flex justify-center items-center py-12 px-[5%] md:bg-[#F4F7FF]">
                 <div className="flex flex-col gap-10 items-center sm:py-24 px-[3%] sm:px-24 bg-white rounded-2xl sm:drop-shadow-lg gap-10">
-                    {searchResults.length >= 1 && (<div className="font-bold text-secondary text-3xl">search results for &quot;{searchParams.get("search")}&quot; on events</div>)}
+                    <div className="w-full items-center px-16 relative flex flex-row">
+                        <input onChange={(e)=> setSearch(e.target.value)} type="text" placeholder="Search Event.." className="input input-bordered w-full drop-shadow-lg px-12 text-black" />
+                        <div className="dropdown dropdown-bottom dropdown-end">
+                            <label tabIndex={0} className="btn m-1 drop-shadow-lg"><BsFilterLeft size={28} className="text-white"/></label>
+                            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 flex flex-col gap-1">
+                                <label>Sort</label>
+                                <div>
+                                    <label className="text-secondary font-medium">SortBy:</label>
+                                    <div className="flex gap-1">
+                                        <button onClick={(e)=> setSortBy(e.target.value)} value={'ASC'} className="btn btn-active w-[70px] normal-case text-white">ASC</button>
+                                        <button onClick={(e)=> setSortBy(e.target.value)} value={'DESC'} className="btn btn-active w-[70px] normal-case text-white">DESC</button>
+                                    </div>
+                                </div>
+                                <label className="text-secondary font-medium">Limit:</label>
+                                <select className="select select-ghost w-full max-w-xs text-primary" onChange={(e)=> setLimit(e.target.value)}>
+                                    <option>5</option>
+                                    <option>10</option>
+                                    <option>15</option>
+                                    <option>20</option>
+                                </select>
+                            </ul>
+                        </div>
+                        <FiSearch size={25} className="text-neutral absolute left-[80px] top-3"/>
+                    </div>
+                    {!search ? <div>{searchResults.length >= 1 && (<div className="font-bold text-secondary text-3xl">search results for &quot;{searchParams.get("search")}&quot; on events</div>)}</div> 
+                    : <div>{searchResults.length >= 1 && (<div className="font-bold text-secondary text-3xl">search results for &quot;{search}&quot; on events</div>)}</div>}
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-6">
                     {searchResults.map(event =>{
                         return(
